e62ec03634bae51d8a21257b912c2d9b9963e024
Htet Khant Linn <htetkhantlinn123@gmail.com>
2026-05-18 16:17:39 +0630

    Uncomment Graph implementation and demo
    Restore the Graph class and its methods (add_vertex, add_edge, remove_edge, remove_vertex) by removing comment markers so the implementation is executable. Also uncomment the example graph creation and print statements, and enable the "After remove vertex D" output and removal call to demonstrate behavior. No logic changes were made — this just activates previously commented code.

--- In-Class Exercise/Week_13_coding_graph_algorithm_traversal/graph_2.py
+++ In-Class Exercise/Week_13_coding_graph_algorithm_traversal/graph_2.py
@@ -1,57 +1,57 @@
-# class Graph: # Undirected unweighed using adjacency list
-#     def __init__(self):
-#         self.adjacency_list = {} # Creating empty dict
+class Graph: # Undirected unweighed using adjacency list
+    def __init__(self):
+        self.adjacency_list = {} # Creating empty dict
 
-#     def add_vertex(self, vertex): # { A: [] }
-#         if vertex not in self.adjacency_list.keys():
-#             self.adjacency_list[vertex] = []
-#             return True
-#         return False
+    def add_vertex(self, vertex): # { A: [] }
+        if vertex not in self.adjacency_list.keys():
+            self.adjacency_list[vertex] = []
+            return True
+        return False
     
-#     def print_graph(self):
-#         for vertex in self.adjacency_list:
-#             print(vertex, ":", self.adjacency_list[vertex])
+    def print_graph(self):
+        for vertex in self.adjacency_list:
+            print(vertex, ":", self.adjacency_list[vertex])
 
-#     def add_edge(self, vertex1, vertex2):
-#         if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
-#             self.adjacency_list[vertex1].append(vertex2)
-#             self.adjacency_list[vertex2].append(vertex1)
-#             return True
-#         return False
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+            self.adjacency_list[vertex1].append(vertex2)
+            self.adjacency_list[vertex2].append(vertex1)
+            return True
+        return False
     
-#     def remove_edge(self, vertex1, vertex2):
-#         if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
-#             try:
-#                 self.adjacency_list[vertex1].remove(vertex2)
-#                 self.adjacency_list[vertex2].remove(vertex1)
-#             except ValueError:
-#                 pass
-#             return True
-#         return False
+    def remove_edge(self, vertex1, vertex2):
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+            try:
+                self.adjacency_list[vertex1].remove(vertex2)
+                self.adjacency_list[vertex2].remove(vertex1)
+            except ValueError:
+                pass
+            return True
+        return False
     
-#     def remove_vertex(self, vertex):
-#         if vertex in self.adjacency_list.keys():
-#             for other_vertex in self.adjacency_list[vertex]: # remove vertex D from neignbour A, C
-#                 self.adjacency_list[other_vertex].remove(vertex) # remove the edge first
-#             del self.adjacency_list[vertex] # after remove the edge and delete the vertex
-#             return True
-#         return False
+    def remove_vertex(self, vertex):
+        if vertex in self.adjacency_list.keys():
+            for other_vertex in self.adjacency_list[vertex]: # remove vertex D from neignbour A, C
+                self.adjacency_list[other_vertex].remove(vertex) # remove the edge first
+            del self.adjacency_list[vertex] # after remove the edge and delete the vertex
+            return True
+        return False
 
 
-# custom_graph = Graph()
-# custom_graph.add_vertex("A")
-# custom_graph.add_vertex("B")
-# custom_graph.add_vertex("C")
-# custom_graph.add_vertex("D")
-# custom_graph.add_edge("A", "B")
-# custom_graph.add_edge("A", "C")
-# custom_graph.add_edge("A", "D")
-# custom_graph.add_edge("B", "C")
-# custom_graph.add_edge("C", "D")
-# custom_graph.print_graph()
+custom_graph = Graph()
+custom_graph.add_vertex("A")
+custom_graph.add_vertex("B")
+custom_graph.add_vertex("C")
+custom_graph.add_vertex("D")
+custom_graph.add_edge("A", "B")
+custom_graph.add_edge("A", "C")
+custom_graph.add_edge("A", "D")
+custom_graph.add_edge("B", "C")
+custom_graph.add_edge("C", "D")
+custom_graph.print_graph()
 
-# # custom_graph.remove_edge("A", "C")
-# # print('After remove edge')
-# print("After remove vertex D")
-# custom_graph.remove_vertex("D")
-# custom_graph.print_graph()
+# custom_graph.remove_edge("A", "C")
+# print('After remove edge')
+print("After remove vertex D")
+custom_graph.remove_vertex("D")
+custom_graph.print_graph()
